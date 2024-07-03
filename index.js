9e07704bdf188ae0a3201716a4a26c46d3c850cb
DevBrianHQ <kipchumbabrian47@gmail.com>
2024-07-03 12:54:26 +0300

    passed test

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        debugger;  
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+}
+
+debugger;  
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+
+// function countDown(number) {
+//     for (let i = number; i >= 0; i--){
+//         console.log(i)
+//     }
+// }
+
+// countDown(10)
+
+function countDown(number){
+    let i = number;
+    while (i >= 0){
+        console.log(i--)
+    }
+}
+countDown(10)
